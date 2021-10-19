6b043527483b35b416500d1fcefc94d8f44bd6ec
Benny Blockchain <ben@styng.social>
2021-10-19 12:59:53 -0400

    btc-tx-searh count subcommand init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 extern crate clap;
-use clap::App;
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    App::new("btc-tx-search").version("0.0.1").get_matches();
+    let matches = App::new("btc-tx-search")
+        .version("0.0.1")
+        .author("Ben Schroth <ben@styng.social")
+        .about("A Bitcoin CLI tool that can search transactions by address or txid.")
+        .subcommand(
+            SubCommand::with_name("account")
+                .about("Searches UTXO set of a provided Account.")
+                .arg(
+                    Arg::with_name("pubkey")
+                        .help("The account pubkey to search.")
+                        .index(1)
+                        .required(true)
+                )
+        )
+        .get_matches();
+
+    if matches.is_present("account") {
+        println!("Searching UTXO set of pubkey...");
+    }
+
+    if let Some(matches) = matches.subcommand_matches("account") {
+        println!("UTXO set of: {}", matches.value_of("pubkey").unwrap());
+    }
+    
 }
